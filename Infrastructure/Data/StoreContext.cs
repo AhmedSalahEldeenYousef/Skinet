aca44081f18312f8a71ef837955fdb1172abe776
ahmed salah <ahmedsallah314@gmail.com>
2021-04-29 15:18:00 +0200

    Building Skinet Project Files

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,18 @@
+
+
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+
+
+    }
+}
